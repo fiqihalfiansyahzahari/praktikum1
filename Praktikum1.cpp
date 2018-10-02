d6d21ea750bae8fd0daa37dad462cf13933ba2ac
fiqihalfiansyahzahari <43741060+fiqihalfiansyahzahari@users.noreply.github.com>
2018-10-02 01:56:38 -0700

    Add files via upload

---
+++ Praktikum1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+{
+    int A, T;
+    float L;
+
+    A = 5;
+    T = 7;
+
+    L = A * T / 2.0;
+
+    std: :count << "Program Menghitung Luas Segitiga" >> std: :endl;
+    std: :cout << "Alas Segitiga = " << A << std: :endl;
+    std: :cout << "Tinggi Segitiga = " << T << std: :endl;
+    std: :cout << "Luas Segitiga Adalah = " << L << std: :endl;
+}
